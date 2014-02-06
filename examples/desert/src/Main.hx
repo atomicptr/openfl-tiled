b31d6fda2a59043cdaaa47e557cb0c94c6d27aa2
Christopher Kaster <ikasoki@gmail.com>
2014-02-06 07:54:33 +0100

    add some basic scrolling functionality to desert sample

--- examples/desert/src/Main.hx
+++ examples/desert/src/Main.hx
@@ -2,17 +2,35 @@ package;
 
 import flash.display.Bitmap;
 import flash.Lib;
+import flash.ui.Keyboard;
+import flash.events.KeyboardEvent;
 
 import openfl.tiled.TiledMap;
 
-import openfl.Assets;
 import openfl.display.FPS;
 
 class Main {
+
+	private static var mapBitmap:Bitmap;
+
 	public static function main():Void {
 		var map = TiledMap.fromAssets("desert.tmx");
-		
-		Lib.current.stage.addChild(new Bitmap(map.createBitmapData()));
+
+		mapBitmap = new Bitmap(map.createBitmapData());
+
+		Lib.current.stage.addChild(mapBitmap);
 		Lib.current.stage.addChild(new FPS());
+
+		Lib.current.stage.addEventListener(KeyboardEvent.KEY_DOWN, onKeyDown);
+	}
+
+	private static function onKeyDown(e:KeyboardEvent):Void {
+		if(e.keyCode == Keyboard.RIGHT) {
+			mapBitmap.x -= 5;
+		}
+
+		if(e.keyCode == Keyboard.DOWN) {
+			mapBitmap.y -= 5;
+		}
 	}
 }
