ef75fce191f224f95804a0904f92fffbfb9fcadd
Christopher Kaster <ikasoki@gmail.com>
2014-02-24 21:13:24 +0100

    add image_layer example

---
+++ examples/image_layer_desert/src/Main.hx
@@ -0,0 +1,37 @@
+package;
+
+import flash.display.Bitmap;
+import flash.Lib;
+import flash.ui.Keyboard;
+import flash.events.KeyboardEvent;
+
+import openfl.tiled.TiledMap;
+
+import openfl.display.FPS;
+
+class Main {
+
+	private static var map:TiledMap;
+
+	public static function main():Void {
+		map = TiledMap.fromAssets("desert.tmx");
+
+		map.x = 0;
+		map.y = 0;
+
+		Lib.current.stage.addChild(map);
+		Lib.current.stage.addChild(new FPS());
+
+		Lib.current.stage.addEventListener(KeyboardEvent.KEY_DOWN, onKeyDown);
+	}
+
+	private static function onKeyDown(e:KeyboardEvent):Void {
+		if(e.keyCode == Keyboard.RIGHT) {
+			map.x -= 5;
+		}
+
+		if(e.keyCode == Keyboard.DOWN) {
+			map.y -= 5;
+		}
+	}
+}
