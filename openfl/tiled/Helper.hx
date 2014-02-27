502e95040389d452a9d13a9c253f533d882027c7
Christopher Kaster <ikasoki@gmail.com>
2014-02-27 16:36:07 +0100

    add path functions for helper

--- openfl/tiled/Helper.hx
+++ openfl/tiled/Helper.hx
@@ -1,37 +1,35 @@
 // Copyright (C) 2013 Christopher "Kasoki" Kaster
-// 
+//
 // This file is part of "openfl-tiled". <http://github.com/Kasoki/openfl-tiled>
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
 // to deal in the Software without restriction, including without limitation
 // the rights to use, copy, modify, merge, publish, distribute, sublicense,
 // and/or sell copies of the Software, and to permit persons to whom the
 // Software is furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included
 // in all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
 // OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 package openfl.tiled;
 
 import flash.display.BitmapData;
 
-import openfl.Assets;
-
 class Helper {
 
 	private static var assetLoader:AssetLoader;
-	
+
 	private function new() {
 	}
-	
+
 	/** This method checks if the given Xml element is really a Xml element! */
 	public static function isValidElement(element:Xml):Bool {
 		return Std.string(element.nodeType) == "element";
@@ -41,25 +39,67 @@ class Helper {
 	public static function setAssetLoader(assetLoader:AssetLoader):Void {
 		Helper.assetLoader = assetLoader;
 	}
-	
-	/** This methods is wrapper for Assets.getText(string), if 
+
+	/** This methods is wrapper for Assets.getText(string), if
 		you're using another Asset managment system simply override this method */
 	public static function getText(assetPath:String):String {
 		if (assetLoader == null) {
 			assetLoader = new DefaultAssetLoader();
 		}
-		
+
 		return assetLoader.getText(assetPath);
 	}
 
-	/** This methods is wrapper for Assets.getBitmapData(string), if 
+	/** This methods is wrapper for Assets.getBitmapData(string), if
 		you're using another Asset managment system simply override this method */
 	public static function getBitmapData(assetPath:String):BitmapData {
 		if(assetLoader == null) {
 			assetLoader = new DefaultAssetLoader();
 		}
-		
+
 		return assetLoader.getBitmapData(assetPath);
 	}
-	
+
+	public static function joinPath(path1:String, path2:String):String {
+		return normalizePath(path1 + path2);
+	}
+
+	public static function normalizePath(path:String):String {
+		var BLANK = '';
+		var SLASH = '/';
+		var DOT = '.';
+		var DOTS = DOT + DOT;
+
+		if(path == null || path == SLASH) {
+			return SLASH;
+		}
+
+		var prependSlash = (path.charAt(0) == SLASH ||
+			path.charAt(0) == DOT);
+		var target = [];
+		var src;
+		var parts;
+		var token;
+
+		src = path.split(SLASH);
+
+		for(i in 0...src.length) {
+			token = src[i];
+
+			if(token == DOTS) {
+				target.pop();
+			} else if(token != BLANK && token != DOT) {
+				target.push(token);
+			}
+		}
+
+		var regex = ~/[\/]{2,}/g;
+
+		var tmp = target.join(SLASH);
+
+		var result = regex.replace(tmp, SLASH);
+
+		return (prependSlash ? SLASH : BLANK) + result;
+	}
+
 }
